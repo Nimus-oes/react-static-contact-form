b494d389f838bff1874542c5ed97dae6da7cd754
Nimus-oes <sseo.code@gmail.com>
2025-03-08 08:00:17 +0900

    Create component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import Logo from "./components/Logo";
+import Title from "./components/Title";
+import Subtitle from "./components/Subtitle";
+import Form from "./components/Form";
+
 export default function App() {
-  return <h1>Hello, World</h1>;
+  return (
+    <>
+      <Logo />
+      <Title />
+      <Subtitle />
+      <Form />
+    </>
+  );
 }
---
+++ src/components/Subtitle.jsx
@@ -0,0 +1,3 @@
+export default function Subtitle() {
+  return <p>Subtitle</p>;
+}
