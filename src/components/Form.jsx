b9c753ac59e870712c9640c78243a33b34cdb58c
Nimus-oes <sseo.code@gmail.com>
2025-03-08 08:58:37 +0900

    Add responsive layout to the form
    A flexbox is applied to switch between column and row

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,15 @@
 export default function Form() {
   return (
-    <form>
-      <div className="form-item name">
+    <form className="form">
+      <div className="form-item half-w">
         <label htmlFor="name">Name</label>
         <input type="text" name="name" id="name" />
       </div>
-      <div className="form-item email">
+      <div className="form-item half-w">
         <label htmlFor="">Company Email</label>
         <input type="email" name="email" id="email" />
       </div>
-      <div className="form-item size">
+      <div className="form-item half-w">
         <label htmlFor="size">Company Size</label>
         <select name="size" id="size">
           <option value="sm">50-100 employees</option>
@@ -17,7 +17,7 @@ export default function Form() {
           <option value="lg">Over 200 employees</option>
         </select>
       </div>
-      <div className="form-item subject">
+      <div className="form-item half-w">
         <label htmlFor="subject">Subject</label>
         <select name="subject" id="subject">
           <option value="landing">Building Landing Pages</option>
@@ -25,16 +25,16 @@ export default function Form() {
           <option value="other">Others</option>
         </select>
       </div>
-      <div className="form-item message">
+      <div className="form-item full-w">
         <label htmlFor="message">Message</label>
         <textarea name="message" id="message"></textarea>
       </div>
-      <div className="form-item consent">
+      <div className="form-item consent full-w">
         <label htmlFor="consent"></label>
         <input type="checkbox" name="consent" id="consent" /> I consent to being
         contacted by the team.
       </div>
-      <button>Contact Sales</button>
+      <button className="full-w">Contact Sales</button>
     </form>
   );
 }
